0ec82cbba2dbbc86a72445efa3c6c72d4a30d6fc
nathanmersha@gmail.com <nathanmersha@gmail.com>
2022-05-24 12:16:28 +0300

    added origins, removed cred

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ app.include_router(schedule.router)
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
