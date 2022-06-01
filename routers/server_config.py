9d83cedc17734789b44d7df08936c79a07b032a5
nathanmersha@gmail.com <nathanmersha@gmail.com>
2022-06-01 12:17:54 +0300

    test

--- routers/server_config.py
+++ routers/server_config.py
@@ -17,9 +17,9 @@ config_id = config["secrets"]["config_id"]
 configModelDal = ConfigModelDAL()
 
 @router.put("/")
-async def update_config(request:Request, updateConfig:ConfigModel, token:str=Header(None)):
-    config_query = {"id" : config_id}
-    configModelDal.update(query=config_query, update_data=updateConfig)
+async def update_config(token:str=Header(None)):
+    # config_query = {"id" : config_id}
+    # configModelDal.update(query=config_query, update_data=updateConfig)
     return {"message" : "config updated"}
 
 @router.get("/")
