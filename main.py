f3db0c1f5009cf0759e0442c8021598fecd880f0
nathanmersha@gmail.com <nathanmersha@gmail.com>
2022-06-01 12:09:08 +0300

    allowed only from mmserver

--- main.py
+++ main.py
@@ -48,13 +48,13 @@ app.include_router(blocklist.router)
 app.include_router(search.router)
 app.include_router(schedule.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.middleware("http")
 async def validate_token(request: Request, call_next):
