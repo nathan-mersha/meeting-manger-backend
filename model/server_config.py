14d77fb3bf1467b7fc2f4ea81f896655c3316b6a
nathanmersha@gmail.com <nathanmersha@gmail.com>
2022-06-03 13:44:40 +0300

    added promo period

--- model/server_config.py
+++ model/server_config.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from distutils.command.config import config
 from pydantic import BaseModel
 from typing import Optional
 
@@ -7,6 +8,7 @@ class ConfigModel(BaseModel):
     id: Optional[str] = None
     tokenExpirationInDay: Optional[int] = 60 #default 60 days 
     pricingPlan : dict
+    promoPeriod : Optional[int] = 1
     firstModified: Optional[datetime] = datetime.now()
     lastModified: Optional[datetime] = datetime.now()
 
@@ -16,6 +18,7 @@ class ConfigModel(BaseModel):
             id=config_json["id"],
             tokenExpirationInDay=config_json["tokenExpirationInDay"],
             pricingPlan=config_json["pricingPlan"],
+            promoPeriod=config_json["promoPeriod"],
             firstModified=config_json["firstModified"],
             lastModified=config_json["lastModified"]
         )
@@ -26,6 +29,7 @@ class ConfigModel(BaseModel):
         if self.id != None: load["id"] = self.id
         if self.tokenExpirationInDay != None: load["tokenExpirationInDay"] = self.tokenExpirationInDay
         if self.pricingPlan != None: load["pricingPlan"] = self.pricingPlan
+        if self.promoPeriod != None: load["promoPeriod"] = self.promoPeriod
         if self.firstModified != None: load["firstModified"] = self.firstModified
         if self.lastModified != None: load["lastModified"] = self.lastModified
 
